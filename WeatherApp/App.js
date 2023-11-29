5aa16ae3e8eec60fec910f9b0bdb1adacb32b39a
nnmcgovern <48661257+nnmcgovern@users.noreply.github.com>
2023-11-29 18:18:55 -0500

    home screen

--- WeatherApp/App.js
+++ WeatherApp/App.js
@@ -3,19 +3,5 @@ import { StyleSheet, Text, View } from "react-native";
 import AppNavigation from "./navigation/appNavigation";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <AppNavigation />
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <AppNavigation />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- WeatherApp/navigation/appNavigation.js
+++ WeatherApp/navigation/appNavigation.js
@@ -1,10 +1,25 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, LogBox } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "../screens/HomeScreen";
+
+const Stack = createNativeStackNavigator();
+
+LogBox.ignoreLogs([
+  "Non-serializable values were found in the navigation state",
+]);
 
 export default function AppNavigation() {
   return (
-    <View>
-      <Text>App hello</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          options={{ headerShown: false }}
+          component={HomeScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
