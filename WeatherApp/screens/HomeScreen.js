16952ef0d90afd95d5c1346de09ef9097782fc52
nnmcgovern <48661257+nnmcgovern@users.noreply.github.com>
2023-12-01 14:49:14 -0500

    tailwind working

--- WeatherApp/screens/HomeScreen.js
+++ WeatherApp/screens/HomeScreen.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, Image } from "react-native";
 
 export default function HomeScreen() {
   return (
     <SafeAreaView>
-      <Text>Home Screen</Text>
+      <Text className="text-red-500 z-0">Home Screen</Text>
+      <Image source={require("../assets/sky.jpeg")} className="h-full w-full" />
     </SafeAreaView>
   );
 }
